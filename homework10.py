da787fb090fb7cfd2a81ce785def5d5f4060e60a
Alexey-Arzhannikov <168473325+Alexey-Arzhannikov@users.noreply.github.com>
2024-05-02 19:00:35 +0300

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,9 @@
+def print_params(test):             # Функция выводит на экран тип параметра и его значение два раза
+    print(type(test))
+    print(test)
+    print(test)
+
+
+print_params('dog')
+print_params(123)
+print_params([3, 7])
